7de3257d553a836e049e96c1cdf93dcc465b03ac
Hoang <phuvanhoang@gmail.com>
2024-10-30 08:46:36 +0700

    set up library

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  //base: "/npm-experiment/",
-  base: "https://github.com/Hoangphuvan/npm-experiment.git",
+  base: "/npm-experiment/",
 });
