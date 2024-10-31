2037ccbfada9df3c05e45e1249eadd71c44e46ea
Hoang <phuvanhoang@gmail.com>
2024-10-31 10:14:33 +0700

    modified api teste

--- tests/api.test.js
+++ tests/api.test.js
@@ -4,11 +4,6 @@ test("GitHub API returns the right response", async () => {
   const data = await fetch(
     "https://api.github.com/repos/Hoangphuvan/npm-experiment/contents/test.txt",
   ).then((res) => res.json());
-  // A year has 52 weeks
-  //console.log(data);
-  //expect(data).toHaveLength(52);
-  //expect(Object.keys(data[0])).toEqual(["total", "week", "days"]);
-  //expect(Object.keys(data.name)).toEqual("test.txt");
   expect(data.name).toBe("test.txt");
   expect(data.path).toBe("test.txt");
 });
